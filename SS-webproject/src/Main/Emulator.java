399d246c66cad9c912badd3213864975e50da5c1
john <johnathon.c.hare@gmail.com>
2019-11-23 17:33:49 -0600

    your new emu

--- SS-webproject/src/Main/Emulator.java
+++ SS-webproject/src/Main/Emulator.java
@@ -1,5 +1,4 @@
 package Main;
-import com.google.gson.*;
 
 
 public class Emulator {
@@ -9,35 +8,35 @@ public class Emulator {
 	private DCApplianceArray dc;
 	final int arraySize = 10;
 	
-	Emulator ()
+	public Emulator ()
 	{
 		panel = new SolarPanel();
 		battery = new Battery();
-		ac = new ACApplianceArray(10);
-		dc = new DCApplianceArray(10);
+		ac = new ACApplianceArray(arraySize);
+		dc = new DCApplianceArray(arraySize);
 		
 	}
 
-	boolean setPanel(int usePerHour, int hoursOfUse)
+	public boolean setPanel(int usePerHour, int hoursOfUse)
 	{
 		return panel.setUsePerHourSetHoursOfUse(usePerHour, hoursOfUse);
 	}
 	
-	boolean setBattery(int min, int max, int current)
+	public boolean setBattery(int min, int max, int current)
 	{
 		return battery.setMinMaxCurrent(min, max, current);
 	}
 	
-	boolean addACAppliance(int usePerHour, int hoursOfUse)
+	public boolean addACAppliance(int usePerHour, int hoursOfUse)
 	{
 		return ac.addACAppliance(usePerHour, hoursOfUse);
 	}
 	
-	boolean addDCAppliance(int usePerHour, int hoursOfUse)
+	public boolean addDCAppliance(int usePerHour, int hoursOfUse)
 	{
 		return dc.addDCAppliance(usePerHour, hoursOfUse);
 	}
-	float[] run(int days)
+	public float[] run(int days)
 	{
 		float[] batteryOverDays = new float[days];
 		for( int i = 0; i < days; i++)
@@ -48,24 +47,37 @@ public class Emulator {
 		}
 		return batteryOverDays;
 	}
-	String runJSON(int days)
-	{
-		return new Gson().toJson(run(days));
-		
-	}
+	
 	public static void main(String[] args)
 	{
+		
 		Emulator emu = new Emulator();
 		emu.setPanel(100, 6);
 		emu.setBattery(1000, 2000, 1500);
-		emu.addACAppliance(30, 4);
-		emu.addDCAppliance(190,3);
-		float[] runOverDays = emu.run(10);
+		emu.addACAppliance(95, 6);
+		float[] runOverDays = emu.run(100);
 		for(int i=0;i<runOverDays.length;i++)
 			System.out.println(runOverDays[i]);
 		
-		
 	}
+	
+	/*
+	 * Emulator emu = new Emulator();
+						emu.setPanel(100, 6);
+						emu.setBattery(1000, 2000, 1500);
+						emu.addACAppliance(30, 4);
+						emu.addDCAppliance(190,3);
+						
+						
+						Gson gsonObj = new Gson();
+							Map<Object,Object> map = null;
+							List<Map<Object,Object>> list = new ArrayList<Map<Object,Object>>();
+							float[] nums = emu.run(10);
+							for(int i = 0; i < nums.length; i++)
+							{
+								map = new HashMap<Object,Object>(); map.put("label", "day" + i); map.put("y", nums[i]); list.add(map);
+							}
+	 */
 	
 	
 	
